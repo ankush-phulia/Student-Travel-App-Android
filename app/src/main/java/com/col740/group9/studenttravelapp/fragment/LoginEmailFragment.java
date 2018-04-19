04257076d54c283135319177a776e80b6d9e6e9a
vikas_chouhan <chouhanvikas95@gmail.com>
2018-04-19 13:25:56 +0530

    Added alternate login to Login UI

---
+++ app/src/main/java/com/col740/group9/studenttravelapp/fragment/LoginEmailFragment.java
@@ -0,0 +1,107 @@
+package com.col740.group9.studenttravelapp.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.col740.group9.studenttravelapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoginEmailFragment.OnLoginEmailFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LoginEmailFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginEmailFragment extends Fragment implements View.OnClickListener{
+
+    private OnLoginEmailFragmentInteractionListener mListener;
+
+    public LoginEmailFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment LoginEmailFragment.
+     */
+    public static LoginEmailFragment newInstance() {
+        LoginEmailFragment fragment = new LoginEmailFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_email, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnLoginEmailFragmentInteractionListener) {
+            mListener = (OnLoginEmailFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    private void sendEmailDetails(){
+        EditText emailEditText = (EditText) getActivity().findViewById(R.id.Email_EditText);
+        CharSequence emailID = emailEditText.getText();
+        if(TextUtils.isEmpty(emailID) || !Patterns.EMAIL_ADDRESS.matcher(emailID).matches()){
+            Toast.makeText(getActivity(), "Enter a valid email address and then press Go", Toast.LENGTH_SHORT).show();
+        }
+        else if (mListener != null) {
+            mListener.onLoginEmailFragmentInteraction(emailID);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_go_email:  sendEmailDetails();
+                                        break;
+        }
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnLoginEmailFragmentInteractionListener {
+
+        void onLoginEmailFragmentInteraction(CharSequence emailID);
+    }
+}
---
+++ app/src/main/java/com/col740/group9/studenttravelapp/fragment/LoginOTPFragment.java
@@ -0,0 +1,110 @@
+package com.col740.group9.studenttravelapp.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.col740.group9.studenttravelapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoginOTPFragment.OnLoginOTPFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LoginOTPFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginOTPFragment extends Fragment implements View.OnClickListener{
+
+    private static final int OTP_LENGTH = 6;
+
+
+    private OnLoginOTPFragmentInteractionListener mListener;
+
+    public LoginOTPFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment LoginOTPFragment.
+     */
+    public static LoginOTPFragment newInstance() {
+        LoginOTPFragment fragment = new LoginOTPFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_otp, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnLoginOTPFragmentInteractionListener) {
+            mListener = (OnLoginOTPFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    private void sendOTPDetails(){
+        EditText otpEditText = (EditText) getActivity().findViewById(R.id.OTP_EditText);
+        CharSequence otp = otpEditText.getText();
+        if(TextUtils.isEmpty(otp) || otp.length() != OTP_LENGTH || !TextUtils.isDigitsOnly(otp)){
+            Toast.makeText(getActivity(), "Enter a valid otp and then press Go", Toast.LENGTH_SHORT).show();
+        }
+        else if (mListener != null) {
+            mListener.onLoginOTPFragmentInteraction(otp);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_go_otp:  sendOTPDetails();
+                break;
+        }
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnLoginOTPFragmentInteractionListener {
+
+        void onLoginOTPFragmentInteraction(CharSequence otp);
+    }
+}
