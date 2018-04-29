53bafe87ad1041862b2c1d4e35e7e72a3c2dc767
ankush-phulia <ankush.phulia22@gmail.com>
2018-04-29 18:07:22 +0530

    Modified user class, populating notifications and user info, as well as stub for updating user info

--- app/src/main/java/com/col740/group9/studenttravelapp/classes/User.java
+++ app/src/main/java/com/col740/group9/studenttravelapp/classes/User.java
@@ -23,4 +23,28 @@ public class User {
         this.sex = user_info_json.getString("sex");
         this.facebook_link = user_info_json.getString("facebook_link");
     }
+
+    public void updateUser(JSONObject user_info_json) throws JSONException {
+        this.id = user_info_json.getInt("id");
+        this.username = user_info_json.getString("username");
+        this.first_name = user_info_json.getString("first_name");
+        this.last_name = user_info_json.getString("last_name");
+        this.email = user_info_json.getString("email");
+        this.password = user_info_json.getString("password");
+        this.sex = user_info_json.getString("sex");
+        this.facebook_link = user_info_json.getString("facebook_link");
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("id", this.id);
+        json.put("username", this.username);
+        json.put("first_name", this.first_name);
+        json.put("last_name", this.last_name);
+        json.put("email", this.email);
+        json.put("password", this.password);
+        json.put("sex", this.sex);
+        json.put("facebook_link", this.facebook_link);
+        return json;
+    }
 }
