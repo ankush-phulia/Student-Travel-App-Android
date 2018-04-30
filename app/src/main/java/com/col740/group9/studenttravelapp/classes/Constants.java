2785dbfcc22cd3309104f58eec4636b82b3b069f
deepak <cs5130281@cse.iitd.ac.in>
2018-05-01 02:19:31 +0530

    Permissions added

--- app/src/main/java/com/col740/group9/studenttravelapp/classes/Constants.java
+++ app/src/main/java/com/col740/group9/studenttravelapp/classes/Constants.java
@@ -1,5 +1,11 @@
 package com.col740.group9.studenttravelapp.classes;
 
+import android.os.Build;
+
 public class Constants {
     public static final String serverURL = "http://10.208.23.177:8000";
+    public static final int RESULT_LOAD_IMAGE = 1;
+    public static final int JOURNEY_TRAVEL_TYPE = 0;
+    public static final int TRIP_TRAVEL_TYPE = 1;
+    public static final int MY_BUILD_VERSION = Build.VERSION.SDK_INT;
 }
